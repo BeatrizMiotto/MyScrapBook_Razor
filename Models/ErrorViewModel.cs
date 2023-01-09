57c87355e900a52389c053b04df4310f61591627
BeatrizMiotto <miottobiasforever@gmail.com>
2023-01-09 12:11:29 -0300

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyScrapBook_Razor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
